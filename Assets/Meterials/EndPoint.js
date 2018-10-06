b7d862ee8f5b32324223f114f50d13a65edd76a6
antu <antu.rex1@gmail.com>
2018-10-06 15:23:28 +0600

    Click Here

---
+++ Assets/Meterials/EndPoint.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var gameMaster : GameMaster;
+
+function OnTriggerEnter (colInfo : Collider)
+{
+	if (colInfo.tag == "Player")
+	{
+		gameMaster.LoadNextLevel();
+	}
+}
---
+++ Assets/Script/GameMaster.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+static var currentScore : int = 0;
+static var finalScore : int = 0;
+
+
+var musicPrefab : Transform;
+
+function Start () {
+	currentScore = 0;
+	
+	if (!GameObject.FindGameObjectWithTag("MM")) {
+		var mManager = Instantiate (musicPrefab, transform.position, Quaternion.identity);
+		mManager.name = musicPrefab.name;
+		DontDestroyOnLoad (mManager);
+	}
+}
+
+
+function LoadNextLevel ()
+{
+	finalScore += currentScore;
+	Application.LoadLevel (Application.loadedLevel + 1);
+}
